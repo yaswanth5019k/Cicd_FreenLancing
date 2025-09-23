a4b43d0c33bd53b00ab9f9464d63cd2b7ac25ad8
anshuganguri <anshuganguri@gmail.com>
2025-09-24 01:34:47 +0530

    added jenkins and fixed run build errors

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -9,6 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      // Add any custom rules here if needed
+    },
+  },
+];
 
 export default eslintConfig;
